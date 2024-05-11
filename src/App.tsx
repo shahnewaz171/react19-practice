846c79c6352af4ae205b1aa11d695d5b84a1371e
shahnewaz171 <shahnewaz601@gmail.com>
2024-05-11 22:14:11 +0600

    learned handle promise with use hook- suspense, form action,useFormStatus, useFormState and useOptimistic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, use } from "react";
+import "./App.css";
+// import Signup from "./Signup";
+
+const fetchData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  return res.json();
+};
+
+const Posts = ({ getData }: { getData: () => Promise<[]> }) => {
+  const posts = use(getData());
+
+  return posts.map((post) => <p className="mt-1">{post.title}</p>);
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center my-10">
+      <p className="text-xl">Posts</p>
+
+      <Suspense
+        fallback={<p className="text-center mt-4 text-2xl">loading...</p>}
+      >
+        <Posts getData={fetchData} />
+      </Suspense>
+
+      {/* <Signup /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Signup.tsx
@@ -0,0 +1,102 @@
+import { useFormStatus } from "react-dom";
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      {pending ? "Submitting" : "Register new account"}
+    </button>
+  );
+};
+
+const Signup = () => {
+  const formAction = async (formData) => {
+    await new Promise((resolved) => setTimeout(resolved, 2000));
+    const data = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    console.log("data", data);
+  };
+
+  return (
+    <form action={formAction} className="max-w-sm mx-auto">
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder="name@flowbite.com"
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your password
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="repeat-password"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Repeat password
+        </label>
+        <input
+          type="password"
+          id="repeat-password"
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          required
+        />
+      </div>
+      <div className="flex items-start mb-5">
+        <div className="flex items-center h-5">
+          <input
+            id="terms"
+            type="checkbox"
+            value=""
+            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+            required
+          />
+        </div>
+        <label
+          htmlFor="terms"
+          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          I agree with the{" "}
+          <a
+            href="#"
+            className="text-blue-600 hover:underline dark:text-blue-500"
+          >
+            terms and conditions
+          </a>
+        </label>
+      </div>
+      <SubmitButton />
+    </form>
+  );
+};
+
+export default Signup;
